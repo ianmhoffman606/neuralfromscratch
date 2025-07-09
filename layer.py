3fd0d407cb56fcb725ac01ac28aea16c15a3b483
Ian Hoffman <imhoffman@wisc.edu>
2025-07-08 22:28:27 -0400

    Enforced types and made all arrays use numpy ndarray

--- layer.py
+++ layer.py
@@ -1,16 +1,18 @@
+import numpy as np
+
 from neuron import Neuron
 
 class Layer:
 
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size: int, output_size: int):
         self.input_size = input_size
         self.output_size = output_size
         self.neurons = []
         for _ in range(output_size):
             self.neurons.append(Neuron(input_size))
 
-    def layer_output(self, inputs):
-        outputs = []
+    def layer_output(self, inputs: np.ndarray) -> np.ndarray:
+        outputs: list[float] = []
         for neuron in self.neurons: 
             outputs.append(neuron.neuron_output(inputs)) 
-        return outputs 
+        return np.array(outputs)
--- neuron.py
+++ neuron.py
@@ -6,9 +6,9 @@ class Neuron:
         self.weights = np.random.rand(input_size)
         self.bias = 0.0
 
-    def neuron_output(self, inputs):
+    def neuron_output(self, inputs: np.ndarray) -> float:
         total = np.dot(self.weights, inputs) + self.bias
         return self.activation_function(total)
     
-    def activation_function(self, x):
+    def activation_function(self, x: float) -> float:
         return np.tanh(x)
